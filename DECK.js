9b5793eae0713b55ccedc16cefe433db918aa6b3
Sara Mathews <smathews120@gmail.com>
2021-07-29 16:59:37 -0500

    I have to commit before merging, this might be a mess

--- DECK.js
+++ DECK.js
@@ -1,231 +1,271 @@
 //object array of all cards in the game
 const Card = [
-    //heart suit
-    {
-        "name": "heartAce",
-        "value": 1,
-        "image": "card-hearts-1.png",
-    },
-    {
-        "name": "heart2",
-        "value": 2,
-        "image": "card-hearts-2.png",
-    },
-    {
-        "name": "heart3",
-        "value": 3,
-        "image": "card-hearts-3.png",
-    },{
-        "name": "heart4",
-        "value": 4,
-        "image": "card-hearts-4.png",
-    },{
-        "name": "heart5",
-        "value": 5,
-        "image": "card-hearts-5.png",
-    },{
-        "name": "heart6",
-        "value": 6,
-        "image": "card-hearts-6.png",
-    },{
-        "name": "heart7",
-        "value": 7,
-        "image": "card-hearts-7.png",
-    },{
-        "name": "heart8",
-        "value": 8,
-        "image": "card-hearts-8.png",
-    },{
-        "name": "heart9",
-        "value": 9,
-        "image": "card-hearts-9.png",
-    },{
-        "name": "heart10",
-        "value": 10,
-        "image": "card-hearts-10.png",
-    },{
-        "name": "heartJester",
-        "value": 11,
-        "image": "card-hearts-11.png",
-    },{
-        "name": "heartQueen",
-        "value": 12,
-        "is OldMaid": false,
-        "image": "card-hearts-12.png",
-    },{
-        "name": "heartKing",
-        "value": 13,
-        "image": "card-hearts-13.png",
-    },
-    //club suit
-    {
-        "name": "clubAce",
-        "value": 1,
-        "image": "card-clubs-1.png",
-    },
-    {
-        "name": "club2",
-        "value": 2,
-        "image": "card-clubs-2.png",
-    },
-    {
-        "name": "club3",
-        "value": 3,
-        "image": "card-clubs-3.png",
-    },{
-        "name": "club4",
-        "value": 4,
-        "image": "card-clubs-4.png",
-    },{
-        "name": "club5",
-        "value": 5,
-        "image": "card-clubs-5.png",
-    },{
-        "name": "club6",
-        "value": 6,
-        "image": "card-clubs-6.png",
-    },{
-        "name": "club7",
-        "value": 7,
-        "image": "card-clubs-7.png",
-    },{
-        "name": "club8",
-        "value": 8,
-        "image": "card-clubs-8.png",
-    },{
-        "name": "club9",
-        "value": 9,
-        "image": "card-clubs-9.png",
-    },{
-        "name": "club10",
-        "value": 10,
-        "image": "card-clubs-10.png",
-    },{
-        "name": "clubJester",
-        "value": 11,
-        "image": "card-clubs-11.png",
-    },{
-        "name": "clubQueen",
-        "value": 12,
-        "is OldMaid": false,
-        "image": "card-clubs-12.png",
-    },{
-        "name": "clubKing",
-        "value": 13,
-        "image": "card-clubs-13.png",
-    },
-    //spade suit
-    {
-        "name": "spadeAce",
-        "value": 1,
-        "image": "card-spades-1.png",
-    },
-    {
-        "name": "spade2",
-        "value": 2,
-        "image": "card-spades-2.png",
-    },
-    {
-        "name": "spade3",
-        "value": 3,
-        "image": "card-spades-3.png",
-    },{
-        "name": "spade4",
-        "value": 4,
-        "image": "card-spades-4.png",
-    },{
-        "name": "spade5",
-        "value": 5,
-        "image": "card-spades-5.png",
-    },{
-        "name": "spade6",
-        "value": 6,
-        "image": "card-spades-6.png",
-    },{
-        "name": "spade7",
-        "value": 7,
-        "image": "card-spades-7.png",
-    },{
-        "name": "spade8",
-        "value": 8,
-        "image": "card-spades-8.png",
-    },{
-        "name": "spade9",
-        "value": 9,
-        "image": "card-spades-9.png",
-    },{
-        "name": "spade10",
-        "value": 10,
-        "image": "card-spades-10.png",
-    },{
-        "name": "spadeJester",
-        "value": 11,
-        "image": "card-spades-11.png",
-    },{
-        "name": "spadeQueen",
-        "value": 12,
-        "is OldMaid": false,
-        "image": "card-spades-12.png",
-    },{
-        "name": "spadeKing",
-        "value": 13,
-        "image": "card-spades-13.png",
-    },
-    //diamond suit
-    {
-        "name": "diamondAce",
-        "value": 1,
-        "image": "card-diamonds-1.png",
-    },
-    {
-        "name": "diamond2",
-        "value": 2,
-        "image": "card-diamonds-2.png",
-    },
-    {
-        "name": "diamond3",
-        "value": 3,
-        "image": "card-diamonds-3.png",
-    },{
-        "name": "diamond4",
-        "value": 4,
-        "image": "card-diamonds-4.png",
-    },{
-        "name": "diamond5",
-        "value": 5,
-        "image": "card-diamonds-5.png",
-    },{
-        "name": "diamond6",
-        "value": 6,
-        "image": "card-diamonds-6.png",
-    },{
-        "name": "diamond7",
-        "value": 7,
-        "image": "card-diamonds-7.png",
-    },{
-        "name": "diamond8",
-        "value": 8,
-        "image": "card-diamonds-8.png",
-    },{
-        "name": "diamond9",
-        "value": 9,
-        "image": "card-diamonds-9.png",
-    },{
-        "name": "diamond10",
-        "value": 10,
-        "image": "card-diamonds-10.png",
-    },{
-        "name": "diamondJester",
-        "value": 11,
-        "image": "card-diamonds-11.png",
-    },{
-        "name": "diamondQueen",
-        "value": 12,
-        "is OldMaid": false,
-        "image": "card-diamonds-12.png",
-    },{
-        "name": "diamondKing",
-        "value": 13,
-        "image": "card-diamonds-13.png",
-    }
-]
+  //heart suit
+  {
+    name: "heartAce",
+    value: 1,
+    image: "card-hearts-1.png",
+  },
+  {
+    name: "heart2",
+    value: 2,
+    image: "card-hearts-2.png",
+  },
+  {
+    name: "heart3",
+    value: 3,
+    image: "card-hearts-3.png",
+  },
+  {
+    name: "heart4",
+    value: 4,
+    image: "card-hearts-4.png",
+  },
+  {
+    name: "heart5",
+    value: 5,
+    image: "card-hearts-5.png",
+  },
+  {
+    name: "heart6",
+    value: 6,
+    image: "card-hearts-6.png",
+  },
+  {
+    name: "heart7",
+    value: 7,
+    image: "card-hearts-7.png",
+  },
+  {
+    name: "heart8",
+    value: 8,
+    image: "card-hearts-8.png",
+  },
+  {
+    name: "heart9",
+    value: 9,
+    image: "card-hearts-9.png",
+  },
+  {
+    name: "heart10",
+    value: 10,
+    image: "card-hearts-10.png",
+  },
+  {
+    name: "heartJester",
+    value: 11,
+    image: "card-hearts-11.png",
+  },
+  {
+    name: "heartQueen",
+    value: 12,
+    "is OldMaid": false,
+    image: "card-hearts-12.png",
+  },
+  {
+    name: "heartKing",
+    value: 13,
+    image: "card-hearts-13.png",
+  },
+  //club suit
+  {
+    name: "clubAce",
+    value: 1,
+    image: "card-clubs-1.png",
+  },
+  {
+    name: "club2",
+    value: 2,
+    image: "card-clubs-2.png",
+  },
+  {
+    name: "club3",
+    value: 3,
+    image: "card-clubs-3.png",
+  },
+  {
+    name: "club4",
+    value: 4,
+    image: "card-clubs-4.png",
+  },
+  {
+    name: "club5",
+    value: 5,
+    image: "card-clubs-5.png",
+  },
+  {
+    name: "club6",
+    value: 6,
+    image: "card-clubs-6.png",
+  },
+  {
+    name: "club7",
+    value: 7,
+    image: "card-clubs-7.png",
+  },
+  {
+    name: "club8",
+    value: 8,
+    image: "card-clubs-8.png",
+  },
+  {
+    name: "club9",
+    value: 9,
+    image: "card-clubs-9.png",
+  },
+  {
+    name: "club10",
+    value: 10,
+    image: "card-clubs-10.png",
+  },
+  {
+    name: "clubJester",
+    value: 11,
+    image: "card-clubs-11.png",
+  },
+  {
+    name: "clubQueen",
+    value: 12,
+    "is OldMaid": false,
+    image: "card-clubs-12.png",
+  },
+  {
+    name: "clubKing",
+    value: 13,
+    image: "card-clubs-13.png",
+  },
+  //spade suit
+  {
+    name: "spadeAce",
+    value: 1,
+    image: "card-spades-1.png",
+  },
+  {
+    name: "spade2",
+    value: 2,
+    image: "card-spades-2.png",
+  },
+  {
+    name: "spade3",
+    value: 3,
+    image: "card-spades-3.png",
+  },
+  {
+    name: "spade4",
+    value: 4,
+    image: "card-spades-4.png",
+  },
+  {
+    name: "spade5",
+    value: 5,
+    image: "card-spades-5.png",
+  },
+  {
+    name: "spade6",
+    value: 6,
+    image: "card-spades-6.png",
+  },
+  {
+    name: "spade7",
+    value: 7,
+    image: "card-spades-7.png",
+  },
+  {
+    name: "spade8",
+    value: 8,
+    image: "card-spades-8.png",
+  },
+  {
+    name: "spade9",
+    value: 9,
+    image: "card-spades-9.png",
+  },
+  {
+    name: "spade10",
+    value: 10,
+    image: "card-spades-10.png",
+  },
+  {
+    name: "spadeJester",
+    value: 11,
+    image: "card-spades-11.png",
+  },
+  {
+    name: "spadeQueen",
+    value: 12,
+    "is OldMaid": false,
+    image: "card-spades-12.png",
+  },
+  {
+    name: "spadeKing",
+    value: 13,
+    image: "card-spades-13.png",
+  },
+  //diamond suit
+  {
+    name: "diamondAce",
+    value: 1,
+    image: "card-diamonds-1.png",
+  },
+  {
+    name: "diamond2",
+    value: 2,
+    image: "card-diamonds-2.png",
+  },
+  {
+    name: "diamond3",
+    value: 3,
+    image: "card-diamonds-3.png",
+  },
+  {
+    name: "diamond4",
+    value: 4,
+    image: "card-diamonds-4.png",
+  },
+  {
+    name: "diamond5",
+    value: 5,
+    image: "card-diamonds-5.png",
+  },
+  {
+    name: "diamond6",
+    value: 6,
+    image: "card-diamonds-6.png",
+  },
+  {
+    name: "diamond7",
+    value: 7,
+    image: "card-diamonds-7.png",
+  },
+  {
+    name: "diamond8",
+    value: 8,
+    image: "card-diamonds-8.png",
+  },
+  {
+    name: "diamond9",
+    value: 9,
+    image: "card-diamonds-9.png",
+  },
+  {
+    name: "diamond10",
+    value: 10,
+    image: "card-diamonds-10.png",
+  },
+  {
+    name: "diamondJester",
+    value: 11,
+    image: "card-diamonds-11.png",
+  },
+  {
+    name: "diamondQueen",
+    value: 12,
+    "is OldMaid": false,
+    image: "card-diamonds-12.png",
+  },
+  {
+    name: "diamondKing",
+    value: 13,
+    image: "card-diamonds-13.png",
+  },
+];
